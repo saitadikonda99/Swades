80040425876d6ad017c915beb470a7cb45436f77
saitadikonda99 <saitadikonda.tech@gmail.com>
2026-02-10 01:03:12 +0530

    feat: Initial project structure and setup

---
+++ backend/src/server.ts
@@ -0,0 +1,21 @@
+import { Hono } from "hono";
+import { serve } from "@hono/node-server";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+
+const PORT = process.env.PORT || 3001;
+
+const app = new Hono();
+
+app.get("/health", (c) => {
+  return c.json({ status: "ok" });
+});
+
+serve({
+  fetch: app.fetch,
+    port: Number(PORT),
+});
+
+console.log(`Server running on http://localhost:${PORT}`);
